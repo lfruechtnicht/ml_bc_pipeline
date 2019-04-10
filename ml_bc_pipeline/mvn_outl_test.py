0e4d6bd350485a0e7c835707ba8b32c9b5850438
Lukas <43547943+lfruechtnicht@users.noreply.github.com>
2019-04-10 19:16:25 +0100

    Update mvn_outl_test.py

--- ml_bc_pipeline/mvn_outl_test.py
+++ ml_bc_pipeline/mvn_outl_test.py
@@ -5,16 +5,7 @@ import numpy.random as nr
 
 
 def main():
-    cov = np.array([[1.0, 0.1, 0.1,],
-                            [0.1, 1.0, 0.1,],
-                            [0.1, 0.1, 1.0]])
-    mu = np.log([0.3, 0.4, 0.5])
-
-    mvn = nr.multivariate_normal(mu, cov, size=400)
-
-    mvn = df(mvn)
-    mvn.loc[15] = [10000000,2020323891,347123904781]
-
+   
     qqplot(mvn)
     stal_plot(mvn)
 
