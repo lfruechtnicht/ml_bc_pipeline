686d086cecf67d0147de05b85246fb76059ba0ef
Lukas <lukas.f@me.com>
2019-04-13 14:13:51 +0200

    new fliled from jaona

--- ml_bc_pipeline/__init__.py
+++ ml_bc_pipeline/__init__.py
@@ -1,20 +1,22 @@
+
 import sys
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from ml_bc_pipeline.data_loader import Dataset
-from ml_bc_pipeline.data_preprocessing import Processor
-from ml_bc_pipeline.feature_engineering import FeatureEngineer
-from ml_bc_pipeline.model import grid_search_MLP, assess_generalization_auprc
+from data_loader import Dataset
+from data_preprocessing import Processor
+from feature_engineering import FeatureEngineer
+from model import grid_search_MLP, assess_generalization_auprc
 
 
 def main():
     #+++++++++++++++++ 1) load and prepare the data
-    file_path = "/Users/fruechtnicht/NOVA/M.Sc_Data_Science_and_Advanced_Analytics/Semester2/Machiene Learning/Project_1/ml_bc_pipeline/ml_project1_data.xlsx"
+    file_path = r"C:\Users\jojo\Documents\Uni\Second Semester\Machine Learning\Project\ml_project1_data.xlsx"
     ds = Dataset(file_path)
 
     #+++++++++++++++++ 2) split into train and unseen
     seed = 0
-    DF_train, DF_unseen = train_test_split(ds.rm_df.copy(), test_size=0.2, stratify=ds.rm_df["DepVar"], random_state=seed)
+    DF_train, DF_unseen = train_test_split(ds.rm_df.copy(), test_size=0.2, stratify=ds.rm_df["Response"], random_state=seed)
+
 
     #+++++++++++++++++ 3) preprocess, based on train
     pr = Processor(DF_train, DF_unseen)
@@ -22,32 +24,39 @@ def main():
     #+++++++++++++++++ 4) feature engineering
     fe = FeatureEngineer(pr.training, pr.unseen)
 
+
     # apply Box-Cox transformations
-    num_features = fe.training._get_numeric_data().drop(['DepVar'], axis=1).columns
-    fe.box_cox_transformations(num_features, target="DepVar")
+    # num_features = fe.training._get_numeric_data().drop(['Response'], axis=1).columns
+    # fe.box_cox_transformations(num_features, target="Response")
 
     # rank input features according to Chi-Squared
-    continuous_flist = fe.box_cox_features
-    categorical_flist = ['DT_MS_Divorced', 'DT_MS_Widow', 'DT_E_Master', 'DT_R_5', 'DT_R_6', "Gender"]
-    fe.rank_features_chi_square(continuous_flist, categorical_flist)
-    print("Ranked input features:\n", fe._rank)
+    # continuous_flist = fe.box_cox_features
+    # categorical_flist = ['DT_MS_Divorced', 'DT_MS_Widow', 'DT_E_Master', 'DT_R_5', 'DT_R_6', "Gender"]
+    # fe.rank_features_chi_square(continuous_flist, categorical_flist)
+    # print("Ranked input features:\n", fe._rank)
 
     # get top n features
-    criteria, n_top = "chisq", 9
-    DF_train_top, DF_unseen_top = fe.get_top(criteria="chisq", n_top=n_top)
+    # criteria, n_top = "chisq", 9
+    # DF_train_top, DF_unseen_top = fe.get_top(criteria="chisq", n_top=n_top)
 
     #+++++++++++++++++ 5) modelling
-    mlp_param_grid = {'mlpc__hidden_layer_sizes': [(3), (6), (3, 3), (5, 5)],
-                      'mlpc__learning_rate_init': [0.001, 0.01]}
-
-    mlp_gscv = grid_search_MLP(DF_train_top, mlp_param_grid, seed)
-    print("Best parameter set: ", mlp_gscv.best_params_)
+    # mlp_param_grid = {'mlpc__hidden_layer_sizes': [(3), (6), (3, 3), (5, 5)],
+    #                   'mlpc__learning_rate_init': [0.001, 0.01]}
+    #
+    # mlp_gscv = grid_search_MLP(DF_train_top, mlp_param_grid, seed)
+    # print("Best parameter set: ", mlp_gscv.best_params_)
     # pd.DataFrame.from_dict(mlp_gscv.cv_results_).to_excel("D:\\PipeLines\\project_directory\\data\\mlp_gscv.xlsx")
 
     #+++++++++++++++++ 6) retraining & assessment of generalization ability
-    auprc = assess_generalization_auprc(mlp_gscv.best_estimator_, DF_unseen_top)
-    print("AUPRC: {:.2f}".format(auprc))
+    # auprc = assess_generalization_auprc(mlp_gscv.best_estimator_, DF_unseen_top)
+    # print("AUPRC: {:.2f}".format(auprc))
 
+
+    #+++++++++++++++++ X, y split
+    y_test = fe.unseen['Response']
+    y_train = fe.training['Response']
+    X_test = fe.unseen.loc[:, fe.unseen.columns != 'Response']
+    X_train = fe.training.loc[:, fe.training.columns != 'Response']
 
 
 if __name__ == "__main__":
--- ml_bc_pipeline/data_preprocessing.py
+++ ml_bc_pipeline/data_preprocessing.py
@@ -23,22 +23,16 @@ class Processor:
         self.training = training #.copy() to mantain a copy of the object
         self.unseen = unseen #.copy() to mantain a copy of the object
 
-        self._drop_constant_features()
-        self._drop_categorical_missing_values()
-
-        num_features = self._filter_df_by_std()
-        self._impute_num_missings_mean(num_features)
+#        self._drop_constant_features()
+#
+#        num_features = self._filter_df_by_std()
+        self._impute_num_missings_mean()
 
     def _drop_constant_features(self):
-        num_df = self.training._get_numeric_data().drop(['DepVar'], axis=1)
+        num_df = self.training.drop(['Response'], axis=1)
         const = num_df.columns[num_df.std() < 0.01]
         self.training.drop(labels=const, axis=1, inplace=True)
         self.unseen.drop(labels=const, axis=1, inplace=True)
-
-    def _drop_categorical_missing_values(self):
-        features = ["Education", "Marital_Status"]
-        self.training.dropna(subset=features, inplace=True)
-        self.unseen.dropna(subset=features, inplace=True)
 
     def _filter_df_by_std(self):
         def _filter_ser_by_std(series_, n_stdev=3.0):
@@ -54,10 +48,72 @@ class Processor:
 
         return list(training_num.columns)
 
-    def _impute_num_missings_mean(self, num_features):
-        self._imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-        X_train_imputed = self._imputer.fit_transform(self.training[num_features].values)
-        X_unseen_imputed = self._imputer.transform(self.unseen[num_features].values)
+    def _impute_num_missings_mean(self):
+        self.training['Income'].fillna((self.training['Income'].mean()), inplace=True)
+        self.unseen['Income'].fillna((self.unseen['Income'].mean()), inplace=True)
+        # self.training = self.training.fillna(self.training.mean())
+        # self.unseen = self.unseen.fillna(self.unseen.mean())
 
-        self.training[num_features] = X_train_imputed
-        self.unseen[num_features] = X_unseen_imputed
+# import sys
+# import numpy as np
+# from sklearn.impute import SimpleImputer
+#
+#
+# class Processor:
+#     """ Performs data preprocessing
+#
+#         The objective of this class is to preprocess the data based on training subset. The
+#         preprocessing steps focus on constant features removal, missing values treatment and
+#         outliers removal and imputation.
+#
+#     """
+#
+#     def __init__(self, training, unseen):
+#         """ Constructor
+#
+#             It is worth to notice that both training and unseen are nothing more nothing less
+#             than pointers, i.e., pr.training is DF_train and pr.unseen is DF_unseen yields True.
+#             If you want them to be copies of respective objects, use .copy() on each parameter.
+#
+#         """
+#         self.training = training #.copy() to mantain a copy of the object
+#         self.unseen = unseen #.copy() to mantain a copy of the object
+#
+#         self._drop_constant_features()
+#         self._drop_categorical_missing_values()
+#
+#         num_features = self._filter_df_by_std()
+#         self._impute_num_missings_mean(num_features)
+#
+#     def _drop_constant_features(self):
+#         num_df = self.training._get_numeric_data().drop(['DepVar'], axis=1)
+#         const = num_df.columns[num_df.std() < 0.01]
+#         self.training.drop(labels=const, axis=1, inplace=True)
+#         self.unseen.drop(labels=const, axis=1, inplace=True)
+#
+#     def _drop_categorical_missing_values(self):
+#         features = ["Education", "Marital_Status"]
+#         self.training.dropna(subset=features, inplace=True)
+#         self.unseen.dropna(subset=features, inplace=True)
+#
+#     def _filter_df_by_std(self):
+#         def _filter_ser_by_std(series_, n_stdev=3.0):
+#             mean_, stdev_ = series_.mean(), series_.std()
+#             cutoff = stdev_ * n_stdev
+#             lower_bound, upper_bound = mean_ - cutoff, mean_ + cutoff
+#             return [True if i < lower_bound or i > upper_bound else False for i in series_]
+#
+#         training_num = self.training._get_numeric_data().drop(["DepVar"], axis=1)
+#         mask = training_num.apply(axis=0, func=_filter_ser_by_std, n_stdev=3.0)
+#         training_num[mask] = np.NaN
+#         self.training[training_num.columns] = training_num
+#
+#         return list(training_num.columns)
+#
+#     def _impute_num_missings_mean(self, num_features):
+#         self._imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+#         X_train_imputed = self._imputer.fit_transform(self.training[num_features].values)
+#         X_unseen_imputed = self._imputer.transform(self.unseen[num_features].values)
+#
+#         self.training[num_features] = X_train_imputed
+#         self.unseen[num_features] = X_unseen_imputed
